9ff082806fba85629c9b20d2974e09ab3ca89e41
xynor <ace.xinxuwang@gmail.com>
2026-03-27 14:40:08 +0800

    f

--- kelpVault/src/kelpVault.sol
+++ kelpVault/src/kelpVault.sol
@@ -2,24 +2,21 @@
 pragma solidity ^0.8.17;
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {IERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC4626Upgradeable.sol";
+import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import {IERC20MetadataUpgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC20MetadataUpgradeable.sol";
 import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import {MathUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 import {SafeERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import {IMintableContract} from "./interfaces/IMintableContract.sol";
 
-contract kelpVault is Initializable, AccessControlUpgradeable, IERC4626Upgradeable {
-    using MathUpgradeable for uint256;
+contract KelpVault is Initializable, AccessControlUpgradeable, ERC4626Upgradeable {
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
-    bytes32 public constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
-    IERC20Upgradeable private _asset;
-    IMintableContract private _kuniBTC;
+    address public redeemContract;
 
     uint256 public totalDeposited;
     uint256 public totalRedeemed;
     uint256 public debt;
-    uint256 public totalUnrealizedProfit;
     uint256 public totalRealizedProfit;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -27,249 +24,65 @@ contract kelpVault is Initializable, AccessControlUpgradeable, IERC4626Upgradeab
         _disableInitializers();
     }
 
-    function initialize(address defaultAdmin, address uniBTC, address kuniBTC) public initializer {
+    function initialize(address _defaultAdmin, address _uniBTC, address _redeemContract) public initializer {
         __AccessControl_init();
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _asset = IERC20Upgradeable(uniBTC);
-        _kuniBTC = IMintableContract(kuniBTC);
+        __ERC20_init("kuniBTC", "kuniBTC");
+        __ERC4626_init(IERC20Upgradeable(_uniBTC));
+        _grantRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
+        redeemContract = _redeemContract;
     }
 
-    function share() public view returns (address) {
-        return address(_kuniBTC);
+    function _transfer(address, address, uint256) internal pure override {
+        revert("TRANSFER_NOT_SUPPORTED");
     }
 
-    function asset() public view virtual override returns (address) {
-        return address(_asset);
+    function totalAssets() public view override returns (uint256) {
+        return totalRealizedProfit + totalDeposited - totalRedeemed;
     }
 
-    function totalAssets() public view virtual override returns (uint256) {
-        return _asset.balanceOf(address(this));
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
+        totalDeposited += assets;
+        super._deposit(caller, receiver, assets, shares);
     }
 
-    function convertToShares(uint256 assets) public view virtual override returns (uint256 shares) {
-        return _convertToShares(assets, MathUpgradeable.Rounding.Down);
-    }
-
-    /**
-     * @dev See {IERC4626-convertToAssets}.
-     */
-    function convertToAssets(uint256 shares) public view virtual override returns (uint256 assets) {
-        return _convertToAssets(shares, MathUpgradeable.Rounding.Down);
-    }
-
-    /**
-     * @dev See {IERC4626-maxDeposit}.
-     */
-    function maxDeposit(address) public view virtual override returns (uint256) {
-        return _isVaultCollateralized() ? type(uint256).max : 0;
-    }
-
-    /**
-     * @dev See {IERC4626-maxMint}.
-     */
-    function maxMint(address) public view virtual override returns (uint256) {
-        return type(uint256).max;
-    }
-
-    /**
-     * @dev See {IERC4626-maxWithdraw}.
-     */
-    function maxWithdraw(address owner) public view virtual override returns (uint256) {
-        return _convertToAssets(_asset.balanceOf(owner), MathUpgradeable.Rounding.Down);
-    }
-
-    /**
-     * @dev See {IERC4626-maxRedeem}.
-     */
-    function maxRedeem(address owner) public view virtual override returns (uint256) {
-        return _asset.balanceOf(owner);
-    }
-
-    /**
-     * @dev See {IERC4626-previewDeposit}.
-     */
-    function previewDeposit(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, MathUpgradeable.Rounding.Down);
-    }
-
-    /**
-     * @dev See {IERC4626-previewMint}.
-     */
-    function previewMint(uint256 shares) public view virtual override returns (uint256) {
-        return _convertToAssets(shares, MathUpgradeable.Rounding.Up);
-    }
-
-    /**
-     * @dev See {IERC4626-previewWithdraw}.
-     */
-    function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, MathUpgradeable.Rounding.Up);
-    }
-
-    /**
-     * @dev See {IERC4626-previewRedeem}.
-     */
-    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
-        return _convertToAssets(shares, MathUpgradeable.Rounding.Down);
-    }
-
-    /**
-     * @dev See {IERC4626-deposit}.
-     */
-    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
-        require(assets <= maxDeposit(receiver), "ERC4626: deposit more than max");
-
-        uint256 shares = previewDeposit(assets);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return shares;
-    }
-
-    /**
-     * @dev See {IERC4626-mint}.
-     *
-     * As opposed to {deposit}, minting is allowed even if the vault is in a state where the price of a share is zero.
-     * In this case, the shares will be minted without requiring any assets to be deposited.
-     */
-    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
-        require(shares <= maxMint(receiver), "ERC4626: mint more than max");
-
-        uint256 assets = previewMint(shares);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return assets;
-    }
-
-    /**
-     * @dev See {IERC4626-withdraw}.
-     */
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
-        require(assets <= maxWithdraw(owner), "ERC4626: withdraw more than max");
-
-        uint256 shares = previewWithdraw(assets);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return shares;
-    }
-
-    /**
-     * @dev See {IERC4626-redeem}.
-     */
-    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
-        require(shares <= maxRedeem(owner), "ERC4626: redeem more than max");
-
-        uint256 assets = previewRedeem(shares);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return assets;
-    }
-
-    /**
-     * @dev Internal conversion function (from assets to shares) with support for rounding direction.
-     *
-     * Will revert if assets > 0, totalSupply > 0 and totalAssets = 0. That corresponds to a case where any asset
-     * would represent an infinite amount of shares.
-     */
-    function _convertToShares(uint256 assets, MathUpgradeable.Rounding rounding)
-        internal
-        view
-        virtual
-        returns (uint256 shares)
-    {
-        uint256 supply = _asset.totalSupply();
-        return (assets == 0 || supply == 0)
-            ? _initialConvertToShares(assets, rounding)
-            : assets.mulDiv(supply, totalAssets(), rounding);
-    }
-
-    /**
-     * @dev Internal conversion function (from assets to shares) to apply when the vault is empty.
-     *
-     * NOTE: Make sure to keep this function consistent with {_initialConvertToAssets} when overriding it.
-     */
-    function _initialConvertToShares(
-        uint256 assets,
-        MathUpgradeable.Rounding /*rounding*/
-    )
-        internal
-        view
-        virtual
-        returns (uint256 shares)
-    {
-        return assets;
-    }
-
-    /**
-     * @dev Internal conversion function (from shares to assets) with support for rounding direction.
-     */
-    function _convertToAssets(uint256 shares, MathUpgradeable.Rounding rounding)
-        internal
-        view
-        virtual
-        returns (uint256 assets)
-    {
-        uint256 supply = _asset.totalSupply();
-        return
-            (supply == 0) ? _initialConvertToAssets(shares, rounding) : shares.mulDiv(totalAssets(), supply, rounding);
-    }
-
-    /**
-     * @dev Internal conversion function (from shares to assets) to apply when the vault is empty.
-     *
-     * NOTE: Make sure to keep this function consistent with {_initialConvertToShares} when overriding it.
-     */
-    function _initialConvertToAssets(
-        uint256 shares,
-        MathUpgradeable.Rounding /*rounding*/
-    )
-        internal
-        view
-        virtual
-        returns (uint256 assets)
-    {
-        return shares;
-    }
-
-    /**
-     * @dev Deposit/mint common workflow.
-     */
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual {
-        // If _asset is ERC777, `transferFrom` can trigger a reenterancy BEFORE the transfer happens through the
-        // `tokensToSend` hook. On the other hand, the `tokenReceived` hook, that is triggered after the transfer,
-        // calls the vault, which is assumed not malicious.
-        //
-        // Conclusion: we need to do the transfer before we mint so that any reentrancy would happen before the
-        // assets are transferred and before the shares are minted, which is a valid state.
-        // slither-disable-next-line reentrancy-no-eth
-        SafeERC20Upgradeable.safeTransferFrom(_asset, caller, address(this), assets);
-        _kuniBTC.mint(receiver, shares);
-
-        emit Deposit(caller, receiver, assets, shares);
-    }
-
-    /**
-     * @dev Withdraw/redeem common workflow.
-     */
     function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
         internal
-        virtual
+        override
     {
-        // If _asset is ERC777, `transfer` can trigger a reentrancy AFTER the transfer happens through the
-        // `tokensReceived` hook. On the other hand, the `tokensToSend` hook, that is triggered before the transfer,
-        // calls the vault, which is assumed not malicious.
-        //
-        // Conclusion: we need to do the transfer after the burn so that any reentrancy would happen after the
-        // shares are burned and after the assets are transferred, which is a valid state.
-        _kuniBTC.burnFrom(owner, shares);
-        SafeERC20Upgradeable.safeTransfer(_asset, receiver, assets);
-
+        totalRedeemed += assets;
+        debt += assets;
+        if (caller != owner) {
+            _spendAllowance(owner, caller, shares);
+        }
+        _burn(owner, shares);
         emit Withdraw(caller, receiver, owner, assets, shares);
+        //call redeem contract to redeem
     }
 
-    /**
-     * @dev Checks if vault is "healthy" in the sense of having assets backing the circulating shares.
-     */
-    function _isVaultCollateralized() private view returns (bool) {
-        return totalAssets() > 0 || _kuniBTC.totalSupply() == 0;
+    function supplyFundsToRedeem(uint256 assets) external onlyRole(OPERATOR_ROLE) {
+        if (assets <= debt) {
+            debt -= assets;
+            SafeERC20Upgradeable.safeTransferFrom(IERC20Upgradeable(super.asset()), msg.sender, redeemContract, assets);
+        } else {
+            if (debt > 0) {
+                SafeERC20Upgradeable.safeTransferFrom(
+                    IERC20Upgradeable(super.asset()), msg.sender, redeemContract, debt
+                );
+            }
+            uint256 remaining = assets - debt;
+            debt = 0;
+            // fat finger
+            totalRealizedProfit += remaining;
+        }
+    }
+
+    function execute(address target, uint256 value, bytes calldata data)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        returns (bytes memory)
+    {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+        require(success, "External call failed");
+        return result;
     }
 }
---
+++ kelpVault/test/kelpVault.t.sol
@@ -0,0 +1,264 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {Test, console} from "forge-std/Test.sol";
+import {uniBTC} from "../src/mocks/uniBTC.sol";
+import {KelpVault} from "../src/kelpVault.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract RedeemMock {
+    // intentionally empty; token receipts are tracked by token balance
+
+    }
+
+contract TargetMock {
+    function ping() external pure returns (uint256) {
+        return 0xBEEF;
+    }
+}
+
+contract KelpVaultTest is Test {
+    uniBTC public uimpl;
+    uniBTC public u;
+    KelpVault public vimpl;
+    KelpVault public v;
+    ProxyAdmin public proxyAdmin;
+
+    address public admin = makeAddr("admin");
+    address public minter = makeAddr("minter");
+    address public operator = makeAddr("operator");
+    address public user = makeAddr("user");
+    RedeemMock public redeem;
+
+    function setUp() public {
+        proxyAdmin = new ProxyAdmin();
+
+        // deploy uniBTC proxy
+        uimpl = new uniBTC();
+        bytes memory dataU = abi.encodeWithSelector(uniBTC.initialize.selector, admin, minter, new address[](0));
+        TransparentUpgradeableProxy proxyU = new TransparentUpgradeableProxy(address(uimpl), address(proxyAdmin), dataU);
+        u = uniBTC(address(proxyU));
+
+        // deploy redeem mock
+        redeem = new RedeemMock();
+
+        // deploy KelpVault proxy
+        vimpl = new KelpVault();
+        bytes memory dataV = abi.encodeWithSelector(KelpVault.initialize.selector, admin, address(u), address(redeem));
+        TransparentUpgradeableProxy proxyV = new TransparentUpgradeableProxy(address(vimpl), address(proxyAdmin), dataV);
+        v = KelpVault(address(proxyV));
+        vm.startPrank(admin);
+        v.grantRole(v.OPERATOR_ROLE(), operator);
+        vm.stopPrank();
+    }
+
+    function testInitializeRoles() public view {
+        assertTrue(v.hasRole(v.DEFAULT_ADMIN_ROLE(), admin));
+        assertTrue(v.hasRole(v.OPERATOR_ROLE(), operator));
+    }
+
+    function testDepositUpdatesTotals() public {
+        // mint to user and deposit
+        vm.prank(minter);
+        u.mint(user, 1000);
+
+        vm.prank(user);
+        u.approve(address(v), 1000);
+
+        vm.prank(user);
+        uint256 shares = v.deposit(500, user);
+
+        assertEq(v.totalDeposited(), 500);
+        assertEq(v.totalAssets(), 500);
+        assertEq(v.balanceOf(user), shares);
+
+        vm.prank(user);
+        vm.expectRevert(bytes("TRANSFER_NOT_SUPPORTED"));
+        v.transfer(address(0xBEEF), 10);
+    }
+
+    function testWithdrawIncreasesDebtAndTotalRedeemed() public {
+        // prepare deposit
+        vm.prank(minter);
+        u.mint(user, 1000);
+        vm.prank(user);
+        u.approve(address(v), 1000);
+        vm.prank(user);
+        v.deposit(500, user);
+        console.log("r", v.convertToAssets(1));
+
+        // withdraw 200
+        vm.prank(user);
+        v.withdraw(200, user, user);
+        console.log("r", v.convertToAssets(1));
+
+        assertEq(v.totalRedeemed(), 200);
+        assertEq(v.debt(), 200);
+    }
+
+    function testSupplyFundsToRedeem_partialCoversDebt() public {
+        // deposit then withdraw to create debt
+        vm.prank(minter);
+        u.mint(user, 1000);
+        vm.prank(user);
+        u.approve(address(v), 1000);
+        vm.prank(user);
+        v.deposit(500, user);
+        vm.prank(user);
+        v.withdraw(300, user, user);
+
+        // operator gets tokens and approves vault
+        vm.prank(minter);
+        u.mint(operator, 300);
+        vm.prank(operator);
+        u.approve(address(v), 150);
+
+        // operator supplies 150 which is <= debt (300)
+        vm.prank(operator);
+        v.supplyFundsToRedeem(150);
+
+        assertEq(v.debt(), 150);
+        assertEq(u.balanceOf(address(redeem)), 150);
+    }
+
+    function testSupplyFundsToRedeem_overageBecomesProfit() public {
+        // create small debt
+        vm.prank(minter);
+        u.mint(user, 1000);
+        vm.prank(user);
+        u.approve(address(v), 1000);
+        vm.prank(user);
+        v.deposit(500, user);
+        vm.prank(user);
+        v.withdraw(100, user, user);
+
+        // operator gets tokens and approves vault
+        vm.prank(minter);
+        u.mint(operator, 400);
+        vm.prank(operator);
+        u.approve(address(v), 400);
+        console.log("r", v.convertToAssets(100_000_000));
+
+        // operator supplies 400 (> debt 100)
+        vm.prank(operator);
+        v.supplyFundsToRedeem(400);
+        // 300 + 500 - 100
+        assertEq(v.debt(), 0);
+        assertEq(v.totalRealizedProfit(), 300);
+        console.log("r", v.convertToAssets(100_000_000));
+        console.log("s", v.previewDeposit(100_000_000));
+        console.log("s", v.previewDeposit(1));
+        console.log("kuniBalance", v.balanceOf(user));
+        console.log("kuniAssets", v.previewRedeem(v.balanceOf(user)));
+        vm.prank(user);
+        v.redeem(100, user, user);
+        // 300 + 500 - 275
+        console.log("assets", v.totalAssets());
+        console.log("r", v.convertToAssets(100_000_000));
+    }
+
+    function testExecuteOnlyAdmin() public {
+        TargetMock target = new TargetMock();
+
+        // admin can call
+        vm.prank(admin);
+        bytes memory res = v.execute(address(target), 0, abi.encodeWithSelector(TargetMock.ping.selector));
+        uint256 out = abi.decode(res, (uint256));
+        assertEq(out, 0xBEEF);
+
+        // non-admin cannot
+        vm.prank(operator);
+        vm.expectRevert();
+        v.execute(address(target), 0, abi.encodeWithSelector(TargetMock.ping.selector));
+    }
+
+    function testSingleUserSequence_printConvertToAssets() public {
+        // single user flow: mint, deposit, withdraw, operator supplies, redeem
+        // initial mint and approve
+        vm.prank(minter);
+        u.mint(user, 1000 * 10000);
+        vm.prank(user);
+        u.approve(address(v), type(uint256).max);
+
+        console.log("initial", v.convertToAssets(100_000_000));
+
+        // deposit 500 * 10000
+        vm.prank(user);
+        v.deposit(500 * 10000, user);
+        console.log("after deposit 5000000", v.convertToAssets(100_000_000));
+
+        // withdraw 200 * 10000
+        vm.prank(user);
+        v.withdraw(200 * 10000, user, user);
+        console.log("after withdraw 2000000", v.convertToAssets(100_000_000));
+
+        // operator supplies <= debt (no profit expected)
+        vm.prank(minter);
+        u.mint(operator, 100 * 10000);
+        vm.prank(operator);
+        u.approve(address(v), 100 * 10000);
+        vm.prank(operator);
+        v.supplyFundsToRedeem(100 * 10000);
+        console.log("after supply 1000000 (<=debt)", v.convertToAssets(100_000_000));
+
+        // operator supplies > debt (creates profit)
+        uint256 prevDebt = v.debt();
+        uint256 supplyAmt = prevDebt + 150 * 10000;
+        vm.prank(minter);
+        u.mint(operator, supplyAmt);
+        vm.prank(operator);
+        u.approve(address(v), supplyAmt);
+        vm.prank(operator);
+        v.supplyFundsToRedeem(supplyAmt);
+        console.log("after supply >debt", v.convertToAssets(100_000_000));
+
+        // redeem some shares
+        uint256 bal = v.balanceOf(user);
+        if (bal > 0) {
+            uint256 redeemShares = bal > 50 ? 50 : bal;
+            vm.prank(user);
+            v.redeem(redeemShares, user, user);
+            console.log("after redeem", v.convertToAssets(100_000_000));
+        }
+
+        // multiple additional rounds to simulate repeated user/operator activity
+        for (uint256 i = 0; i < 8; i++) {
+            uint256 r = uint256(keccak256(abi.encodePacked(i)));
+
+            if (r % 3 == 0) {
+                // user mint + deposit
+                uint256 amt = ((r % 300) + 1) * 10000;
+                vm.prank(minter);
+                u.mint(user, amt);
+                vm.prank(user);
+                v.deposit(amt, user);
+                console.log("round", i, "user deposit", amt);
+                console.log("round", i, v.convertToAssets(100_000_000));
+            } else if (r % 3 == 1) {
+                // user withdraw
+                uint256 amt = ((r % 200) + 1) * 10000;
+                vm.prank(user);
+                v.withdraw(amt, user, user);
+                console.log("round", i, "user withdraw", amt);
+                console.log("round", i, v.convertToAssets(100_000_000));
+            } else {
+                // operator supply
+                uint256 prevDebt = v.debt();
+                uint256 amt = ((r % 500) + 1) * 10000;
+                vm.prank(minter);
+                u.mint(operator, amt);
+                vm.prank(operator);
+                u.approve(address(v), amt);
+                vm.prank(operator);
+                v.supplyFundsToRedeem(amt);
+                console.log("round", i, "operator supply", amt);
+                console.log("---prevDebt", prevDebt, "cur", v.convertToAssets(100_000_000));
+            }
+        }
+    }
+
+    function testPrecison() public view {
+        console.log("deposit", v.previewDeposit(1));
+    }
+}
